029d0e1e2b607b18b1888974928e98d2a0eb1dbf
ariefzuhri <ariefzuhri72@gmail.com>
2020-10-12 14:26:41 +0700

    Commit ulang

--- app/src/main/java/com/udindev/sade/fragment/ProfileFragment.java
+++ app/src/main/java/com/udindev/sade/fragment/ProfileFragment.java
@@ -1,7 +1,15 @@
 package com.udindev.sade.fragment;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,16 +18,11 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.rishabhharit.roundedimageview.RoundedImageView;
 import com.udindev.sade.R;
 import com.udindev.sade.activity.LoginActivity;
 import com.udindev.sade.activity.PusatBantuanActivity;
@@ -28,12 +31,12 @@ import com.udindev.sade.viewmodel.ProfileViewModel;
 
 import static com.udindev.sade.utils.AppUtils.loadImageFromUrl;
 
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends Fragment implements View.OnClickListener{
 
     private FirebaseAuth firebaseAuth;
     private FirebaseUser firebaseUser;
-    private ImageView imgPhoto;
-    private TextView tvName, tvEmail, tvAddress, tvPhoneNumber, tvWaNumber;
+    private RoundedImageView imgPhoto;
+    private TextView tvName, tvAddress, tvFullname, tvEmail, tvFulladdress, tvPhoneNumber, tvWaNumber;
     private ProfileViewModel profileViewModel;
 
     public ProfileFragment() {}
@@ -53,28 +56,70 @@ public class ProfileFragment extends Fragment {
 
         imgPhoto = view.findViewById(R.id.img_photo_profile);
         tvName = view.findViewById(R.id.tv_name_profile);
-        tvEmail = view.findViewById(R.id.tv_email_profile);
         tvAddress = view.findViewById(R.id.tv_address_profile);
+        tvFullname = view.findViewById(R.id.tv_fullname_profile);
+        tvEmail = view.findViewById(R.id.tv_email_profile);
+        tvFulladdress = view.findViewById(R.id.tv_fulladdress_profile);
         tvPhoneNumber = view.findViewById(R.id.tv_phone_number_profile);
         tvWaNumber = view.findViewById(R.id.tv_wa_number_profile);
+
+        TextView tvAbout = view.findViewById(R.id.tv_about_profile);
+        TextView tvHelpCenter = view.findViewById(R.id.tv_help_center_profile);
+        TextView tvLogout = view.findViewById(R.id.tv_logout_profile);
+        tvAbout.setOnClickListener(this);
+        tvHelpCenter.setOnClickListener(this);
+        tvLogout.setOnClickListener(this);
+
+        Button btnEdit = view.findViewById(R.id.btn_edit_profile);
+        btnEdit.setOnClickListener(this);
 
         profileViewModel = new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory()).get(ProfileViewModel.class);
         profileViewModel.getData().observe(getViewLifecycleOwner(), new Observer<Profile>() {
             @Override
             public void onChanged(Profile profile) {
-                loadImageFromUrl(getContext(), imgPhoto, firebaseUser.getPhotoUrl().toString());
+                // Daftar pakai email+pass tidak ada foto
+                Uri photoUrl = firebaseUser.getPhotoUrl();
+                if (photoUrl != null) loadImageFromUrl(getContext(), imgPhoto, photoUrl.toString());
+
                 tvName.setText(firebaseUser.getDisplayName());
-                tvEmail.setText(firebaseUser.getEmail());
                 tvAddress.setText(profile.getAddress());
+                tvFullname.setText(firebaseUser.getDisplayName());
+                tvEmail.setText(firebaseUser.getEmail());
+                tvFulladdress.setText(profile.getAddress());
                 tvPhoneNumber.setText(profile.getPhoneNumber());
                 tvWaNumber.setText(profile.getWaNumber());
             }
         });
+    }
 
-        Button btnLogoutTest = view.findViewById(R.id.btn_logout_test_profile);
-        btnLogoutTest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+    @Override
+    public void onStart() {
+        super.onStart();
+        profileViewModel.loadData(firebaseUser.getUid());
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_edit_profile:
+                Fragment fragment = new EditProfileFragment();
+                if (getActivity() != null) {
+                    getActivity().getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fl_container, fragment)
+                            .addToBackStack(null)
+                            .commit();
+                }
+                break;
+
+            case R.id.tv_about_profile:
+                //startActivity(new Intent(getActivity(), ));
+                break;
+
+            case R.id.tv_help_center_profile:
+                startActivity(new Intent(getActivity(), PusatBantuanActivity.class));
+                break;
+
+            case R.id.tv_logout_profile:
                 GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                         .requestIdToken(getString(R.string.default_web_client_id))
                         .requestEmail()
@@ -84,20 +129,7 @@ public class ProfileFragment extends Fragment {
 
                 startActivity(new Intent(getActivity(), LoginActivity.class));
                 getActivity().finish();
-            }
-        });
-
-        Button btnPusatBantuan;
-        btnPusatBantuan = view.findViewById(R.id.btn_pusat_bantuan);
-        btnPusatBantuan.setOnClickListener(v -> {
-            Intent intent = new Intent(getContext(), PusatBantuanActivity.class);
-            startActivity(intent);
-        });
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        profileViewModel.loadData(firebaseUser.getUid());
+                break;
+        }
     }
 }
