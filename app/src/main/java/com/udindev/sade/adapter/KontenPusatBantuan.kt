39b7c217e9facdae587962c1429c72c805337c1f
rinogen <hubertusrino@gmail.com>
2020-10-19 21:57:04 +0700

    Mengisi pusat bantuan

--- app/src/main/java/com/udindev/sade/adapter/KontenPusatBantuan.kt
+++ app/src/main/java/com/udindev/sade/adapter/KontenPusatBantuan.kt
@@ -3,13 +3,30 @@ package com.udindev.sade.adapter
 object KontenPusatBantuan {
 
     val judulPusatBantuan = arrayListOf(
-            "Bagaimana Transaksi Aman ?",
-            "Bagaimana menambahkan Produk ?"
+            "Bagaimana cara menambahkan produk,jasa, atau usaha di Sade?",
+            "Bagaimana cara membeli di Sade?",
+            "Bagaimana menambahkan favorit di Sade?",
+            "Bagimana melakukan transaksi aman ?"
     )
 
     val deskripsiPusatBantuan = arrayListOf(
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi lorem malesuada interdum sit faucibus donec suspendisse. Adipiscing ac eu ac enim. Nisi diam nunc in laoreet. Vel sagittis eget molestie." ,
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi lorem malesuada interdum sit faucibus donec suspendisse. Adipiscing ac eu ac enim. Nisi diam nunc in laoreet. Vel sagittis eget molestie."
+            "1. Pengguna dapat langsung menuju menu toko saya kemudian ketuk 'Tambahkan Produkmu'.\n" +
+                    "2. Cantumkan nama produk/jasa/usaha yang akan ditawarkan dengan padat dan jelas.\n" +
+                    "3. Pilih kategori sesuai produk/jasa/usaha.\n" +
+                    "4. Masukan alamat mencakup kecamatan, kabupaten, provinsi sesuai lokasi pengguna.\n" +
+                    "5. Masukan nomer whatssap aktif dan sudah terverifikasi.\n" +
+                    "6. Masukan harga seusai yang diingkan dan bersifat opsional jika tidak ingin diisi.\n" +
+                    "7. Tambhakan deskripsi mengenai detail produk/jasa/usaha secara padat dan jelas.\n" +
+                    "9. Pilih gambar yang dapat mendukung deskripisi.\n" +
+                    "10. Klik 'Simpan' " ,
+            "1. Lakukan pencarian pada kolom 'Cari' atau melalui preferensi 'Kategori'.\n" +
+                    "2. Pilih produk/jasa/usaha yang diinginkan.\n" +
+                    "3. Lanjutkan dengan menguhungi penjual melalui WhatsApp",
+            "1. Pilih produk/jasa/usaha yang akan disimpan pada menu favorit.\n" +
+                    "2. Pada menu deskripsi tekan icon 'hati' untuk menambahkan ke favorit.\n" +
+                    "3. Cek pada menu favorit",
+            "Untuk memastikan transaksi aman maka pengguna harus memastikan produk/jasa/usaha tersebut memberikan nomer whatsaap yang " +
+                    "sudah terverifikasi dan dapat dihubungi\n"
     )
 
 }
